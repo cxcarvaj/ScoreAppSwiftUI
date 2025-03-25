097ad672170ab457a23cd7a9a43c0e3600143cd9
cxcarvaj <cxcarvaj@gmail.com>
2025-03-25 11:22:53 -0500

    Initial Commit

---
+++ ScoreAppSwiftUI/ScoreAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreAppSwiftUIApp.swift
+//  ScoreAppSwiftUI
+//
+//  Created by Carlos Xavier Carvajal Villegas on 25/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
